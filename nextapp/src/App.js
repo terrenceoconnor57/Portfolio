85444eace240136d098e9f224ed4f7e8d7bf3bab
terrenceoconnor57 <60070754+terrenceoconnor57@users.noreply.github.com>
2020-04-08 17:29:53 -0400

    First

---
+++ nextapp/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Card, Button, Text } from 'react-bootstrap';
+import logo from './logo.svg';
+import './App.css';
+import Top from './Top/Top';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+
+      <nav className = 'links'>
+
+<ul>
+<li className = 'sitemain'><a>TerrenceSite.com</a></li>
+<li><a>Home</a></li>
+<li><a>Certifications</a></li>
+<li><a>GitHub</a></li>
+</ul>
+</nav>
+      </header>
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ nextapp/src/Top/Top.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Top.css'
+
+
+
+
+
+
+
+const Top = (props) => {
+  return (
+
+<div>
+
+<h2 className = 'a'>AWS</h2>
+<h2 className = 'a'>Azure</h2>
+<h2 className = 'a'>Google</h2>
+</div>
+
+)
+}
+
+
+export default Top;
